56bf3a79e94ee320ccafcdb566f5263cfdf24c52
Sweets Sweetman <sweetmantech@gmail.com>
2024-05-16 22:57:59 -0500

    I add working attestations.

---
+++ lib/attest.tsx
@@ -0,0 +1,24 @@
+import { easAbi } from "./abi/eas";
+import { CHAIN, EAS as EASContractAddress } from "./consts";
+import { whitelistAccount, whitelistClient } from "./clients/whitelistClient";
+
+const attest = async (args: any[]) => {
+  try {
+    console.log("args:", args);
+    const tx = await whitelistClient.writeContract({
+      account: whitelistAccount,
+      address: EASContractAddress,
+      abi: easAbi,
+      chain: CHAIN,
+      functionName: "attest",
+      args,
+    });
+    console.log("Transaction successful:", tx);
+    return tx;
+  } catch (err) {
+    console.error("Error during attestation:", err);
+    return false;
+  }
+};
+
+export default attest;
